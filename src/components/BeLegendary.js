4a1d125265dbf48f91b2d1c3c07ca52f9c58e7df
alohajt <alohajiatao@gmail.com>
2020-05-15 16:04:30 -0600

    slide function draft

--- src/components/BeLegendary.js
+++ src/components/BeLegendary.js
@@ -1,7 +1,38 @@
 import React, { Component } from 'react'
 
+
+
 export default class BeLegendary extends Component{
     state = []
+
+    // slideIndex = 1;
+    // showSlides(slideIndex);
+
+    // // Next/previous controls
+    // plusSlides(n) {
+    // showSlides(slideIndex += n);
+    // }
+
+    // // Thumbnail image controls
+    // currentSlide(n) {
+    //     showSlides(slideIndex = n);
+    // }
+
+    // showSlides(n) {
+    //     var i;
+    //     var slides = document.getElementsByClassName("mySlides");
+    //     var dots = document.getElementsByClassName("dot");
+    //     if (n > slides.length) {slideIndex = 1}
+    //     if (n < 1) {slideIndex = slides.length}
+    //     for (i = 0; i < slides.length; i++) {
+    //         slides[i].style.display = "none";
+    //     }
+    //     for (i = 0; i < dots.length; i++) {
+    //         dots[i].className = dots[i].className.replace(" active", "");
+    //     }
+    //     slides[slideIndex-1].style.display = "block";
+    //     dots[slideIndex-1].className += " active";
+    // }
 
     render(){
         return (
@@ -13,25 +44,40 @@ export default class BeLegendary extends Component{
                 </p>
                 <p>_______________________</p>
                 
+                {/* <!-- Slideshow container --> */}
                 <div className="three-images-container">
-                    <div className="intro-card">
+                    <div className="intro-card fade">
+                        <div className="numbertext">1 / 3</div>
                         <img className="intro-card-image" src="https://playruneterra.com/assets/45ccd50389e0d91555d3d2a6d2d83a2d3ca8e297/static/image/intro-cards/smv-bacon-s001-dem-selfless-shieldguard.jpg"/>
                         <p className="intro-card-title">SKILL ABOVE ALL</p>
                         <p className="intro-card-content">It's all about the choices you make and the risks you take. Every move matters and the outplays are all up to you.</p>
 
                     </div>
 
-                    <div className="intro-card">
+                    <div className="intro-card fade">
+                        <div className="numbertext">2 / 3</div>
                         <img className="intro-card-image" src="https://playruneterra.com/assets/45ccd50389e0d91555d3d2a6d2d83a2d3ca8e297/static/image/intro-cards/S01_U_P14_Eager-Apprentice.jpg"/>
                         <p className="intro-card-title">CRAFT YOUR STRATEGY</p>
                         <p className="intro-card-content">Combine, adapt, and experiment with frequent new releases for every region in an always-evolving meta.</p>
                     </div>
 
-                    <div className="intro-card">
+                    <div className="intro-card fade">
+                        <div className="numbertext">3 / 3</div>
                         <img className="intro-card-image" src="https://playruneterra.com/assets/45ccd50389e0d91555d3d2a6d2d83a2d3ca8e297/static/image/intro-cards/S01_U_F06_Poro.jpg"/>
                         <p className="intro-card-title">YOUR CARDS, YOUR WAY</p>
                         <p className="intro-card-content">Earn cards for free as you play or buy exactly what you want in the store— you’re in control of your collection.</p>
                     </div>
+
+                    {/* <!-- Next and previous buttons --> */}
+                    <a className="prev" onclick="plusSlides(-1)">&#10094;</a>
+                    <a className="next" onclick="plusSlides(1)">&#10095;</a>
+                </div>
+                {/* <!-- The dots/circles --> */}
+                {/* style={"text-align:center"} */}
+                <div >
+                    <span className="dot" onClick="currentSlide(1)"></span>
+                    <span className="dot" onClick="currentSlide(2)"></span>
+                    <span className="dot" onClick="currentSlide(3)"></span>
                 </div>
             </div>
         )
